f90259f257e30f7e3f36e80665234f5814bb885d
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-03-12 17:44:26 +0500

    Removing: :tada: Connect.js from Tree

--- connect.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose
-  .connect("mongodb://localhost/todo", { useNewUrlParser: true })
-  .then(() => console.log("Db Connected"))
-  .catch(err => console.error(err));
