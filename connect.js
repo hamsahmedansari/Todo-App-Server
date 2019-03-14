ab684594dfcd327992b0eac7e7834b89c55e0b4d
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-03-14 15:29:43 +0500

    Fixed: :bug: fixed delete issue by changing to findByIdAndRemove()

--- connect.js
+++ connect.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost/todo", { useNewUrlParser: true })
+  .connect(
+    "mongodb+srv://mongoUser:3mArjW4VSg9iXAKL@todo-app-nodejs-react-b0d3t.mongodb.net/test?retryWrites=true",
+    // "mongodb://localhost/todo",
+    { useNewUrlParser: true }
+  )
   .then(() => console.log("Db Connected"))
   .catch(err => console.error(err));
