bed5a1c5a2c538c84289d99dcf617db783e3f0ab
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-03-21 21:21:16 +0500

    UPDATE: :construction: webPush config

--- routes/subscription/subscription.js
+++ routes/subscription/subscription.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { Subscription } = require("../../models/subscription");
+const webPush = require("web-push");
 // allPost
 router.get("/", async (req, res) => {
   const result = await Subscription.find();
@@ -26,11 +27,32 @@ router.post("/", async (req, res) => {
   });
 
   let result = await obj.save();
-
-  res.status(200).json({
-    message: "Successfully Created Post",
-    data: result
-  });
+  const allResult = await Subscription.find();
+  webPush.setVapidDetails(
+    "mailto:abc@abc.com",
+    "BFxOH3BqZRmBcbqunY0kcv5OqJQt-RxAnRmyPjBIXaCwaHQR0uGd_bFH3D6K8oY4FGNLhMVmqFmoMkZYYHogZU0",
+    "ZwNdctHaY1S27iETsNa9vl9c3xWZL8MTOH_mRH14IoU"
+  );
+  for (let i = 0; i < allResult.length; i++) {
+    let pushConfig = {
+      endpoint: allResult[i].endpoint,
+      keys: {
+        auth: allResult[i].keys.auth,
+        p256dh: allResult[i].keys.p256dh
+      }
+    };
+    webPush.sendNotification(pushConfig, {
+      title: "New Post",
+      content: "New Post Content"
+    });
+  }
+  res
+    .status(200)
+    .json({
+      message: "Successfully Created Post",
+      data: result
+    })
+    .catch(err => console.log(err));
 });
 
 module.exports = router;
