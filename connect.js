002da88550bc5020bee529218f0d76768cfc5f8e
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-03-21 20:18:39 +0500

    UPDATE: :tada: subscription route setup

--- connect.js
+++ connect.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(
-    // "mongodb+srv://mongoUser:3mArjW4VSg9iXAKL@todo-app-nodejs-react-b0d3t.mongodb.net/test?retryWrites=true",
-    "mongodb://localhost/todo",
-    { useNewUrlParser: true }
-  )
+  .connect("mongodb://localhost/todo", { useNewUrlParser: true })
   .then(() => console.log("Db Connected"))
   .catch(err => console.error(err));
